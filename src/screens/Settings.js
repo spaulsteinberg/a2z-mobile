2f4e7c2441b7e712117505d95e0c8101407e6825
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-05-09 13:54:42 -0500

    close #19 close #15 already complete

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,13 +1,38 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Alert, StyleSheet } from 'react-native'
+import { AZButton } from '../components/ui'
 import { logout } from '../firebase/api'
+import Colors from '../styles/Colors'
+
 const Settings = () => {
+
+    const handleLogoutClick = () => {
+        Alert.alert('Logout', 'Are you sure you want to logout?', [
+            { text: 'Back', style: 'cancel' },
+            { text: 'Logout', style: 'destructive', 
+                onPress: () => logout().then(_ => console.log("logged out")).catch(err => console.log(err))}
+        ])
+    }
   return (
-    <View>
-        <Text>Settings</Text>
-        <Button onPress={() => logout().catch(err => console.log(err))} title="Logout" />
+    <View style={styles.container}>
+        <Text>My Profile</Text>
+        <Text>Email Change</Text>
+        <Text>Password Change</Text>
+        <AZButton title="Logout" innerStyle={styles.buttonStyle} onPress={handleLogoutClick} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginHorizontal: 24,
+        marginVertical: 12,
+        justifyContent: 'flex-start'
+    },
+    buttonStyle: {
+        backgroundColor: Colors.error
+    }
+})
 
 export default Settings
