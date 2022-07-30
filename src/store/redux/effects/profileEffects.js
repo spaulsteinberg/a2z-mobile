de87412ba21414749af524a7c72c1ddf4b695556
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-07-29 22:25:14 -0500

    logging out unauth non drivers

--- src/store/redux/effects/profileEffects.js
+++ src/store/redux/effects/profileEffects.js
@@ -1,11 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { Alert } from "react-native";
 import { getAccount } from "../../../firebase/api";
-
+import { logout } from '../../../firebase/api'
 const getProfile = createAsyncThunk(
     'profile/getProfile',
     async (token) => {
         return await getAccount(token)
-        .then(res => Promise.resolve(res.data.data))
+        .then(res => {
+            const { data: { isDriver }} = res.data
+            if (!isDriver) { 
+                return Alert.alert("Unauthorized", "You are not authorized on this app.", [
+                    {
+                        text: 'Logout', style: 'destructive',
+                        onPress: async () => await logout().catch(err => console.log(err))
+                    }
+                ])
+            }
+            return Promise.resolve(res.data.data)
+        })
         .catch(err => {
             console.log(err, err.response)
             return Promise.reject("Something went wrong")
