874fe943b4df760e19a3e6c06ffc615108d1253f
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-08-08 10:44:44 -0500

    change endpoint

--- src/firebase/api/history/getUserApplicationStatus.js
+++ src/firebase/api/history/getUserApplicationStatus.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { URL_ENDPOINT } from "../info";
 
-const getUserApplicationStatus = (token, ticketId) => axios.get(`${URL_ENDPOINT}/history/${ticketId}`, { headers: { token } })
+const getUserApplicationStatus = (token, ticketId) => axios.get(`${URL_ENDPOINT}/history/id/${ticketId}`, { headers: { token } })
 
 export default getUserApplicationStatus
--- src/firebase/api/history/postUserTicketInquiry.js
+++ src/firebase/api/history/postUserTicketInquiry.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { URL_ENDPOINT } from "../info";
 
-const postUserTicketInquiry = (token, ticketId) => axios.post(`${URL_ENDPOINT}/history/${ticketId}`, {}, { headers: { token } })
+const postUserTicketInquiry = (token, ticketId) => axios.post(`${URL_ENDPOINT}/history/id/${ticketId}`, {}, { headers: { token } })
 
 export default postUserTicketInquiry
